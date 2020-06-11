a56dd81ac81639450e2d0ab3a6be851e156ba631
GeorgiiDermenzhy <george.dermenzhy@gmail.com>
2020-06-11 17:28:35 +0200

    Modify task_2 of Module 2 with:
    create CONSTANT variables for weight and implemented in display_inventory function
    update dragonLoot variable changed to dragon_loot
    change header for display_inventory function
    change print iteams , to use default print() separator settings
    change item_total counter to use augmented assignment
    here notify our hero if he/she is carrying too much
    add implementation of defaultdict for skipped dict
    change print for number of added items, to use f-strings

--- tasks/Module 2/task_1.py
+++ tasks/Module 2/task_1.py
@@ -1,8 +1,14 @@
 """Task 1 and 2 of Module 2."""
 
-stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+from collections import defaultdict
+# create CONSTANT variables for weight
+LIGHT_WEIGHT_THRESHOLD = 60
+HEAVY_WEIGHT_THRESHOLD = 70
+MAX_INVENTORY_CAPACITY = 80
 
-dragonLoot = [
+stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+# update dragonLoot variable changed to dragon_loot
+dragon_loot = [
 
     'gold coin', 'chewed gum', 'dagger', 'gold coin', 'gold coin', 'ruby',
 
@@ -10,41 +16,43 @@ dragonLoot = [
 
 
 def display_inventory(inventory):
-    """Display inventory.
-
-    Check the inventory of a player and provides weight/speed dependency status
+    # change header for display_inventory function
+    """Check the inventory of a player and provides weight/speed dependency status
     """
     print("Inventory:")
     item_total = 0
 
     for item_name, item_count in inventory.items():
-        print(item_name + " " + str(item_count))
-        item_total = item_total + item_count
+        # change print iteams , to use default print() separator settings
+        print(item_name, item_count)
+        # change item_total counter to use augmented assignment
+        item_total += 1
 
     print("Total number of items: " + str(item_total))
     # here notify our hero if he/she is carrying too much
-    if 60 >= item_total <= 69:
+    if item_total >= LIGHT_WEIGHT_THRESHOLD:
         print(
             "CAUTION: Your backpack weighs a lot, "
             "your stamina runs out quicker!")
-    elif 70 >= item_total <= 79:
+    elif item_total >= HEAVY_WEIGHT_THRESHOLD:
         print(
             "CAUTION: Your equipment is very heavy, "
             "you're moving slower than usual!")
-    elif item_total > 80:
+    elif item_total >= MAX_INVENTORY_CAPACITY:
         print("CAUTION: You are overloaded, can't move!")
 
 
 def add_to_inventory(inventory, added_items):
-    """Add to inventory.
+    """Add to inventory:
 
-    - Add new items to inventory of a player
+    - Adds new items to inventory of a player
     - Checks input items for trash
     - Provides information on skipped added iteams
     """
-    print("Inventory:")
 
     skipped = {}
+    # add implementation of defaultdict for skipped dict
+    skipped = defaultdict(int)
 
     added_items_counter = 0
 
@@ -53,25 +61,23 @@ def add_to_inventory(inventory, added_items):
 
     for item in added_items:
         if item in trash:
-            if item in skipped:
-                skipped[item] = skipped[item] + 1
-            else:
-                skipped[item] = 1
+            skipped[item] += 1
         elif item in inventory:
-            inventory[item] = inventory[item] + 1
+            inventory[item] += 1
             added_items_counter += 1
         else:
             inventory[item] = 1
             added_items_counter += 1
-
-    print("Added", added_items_counter, "items to the inventory")
+    # change print for number of added items, to use f-strings
+    print(f"Added {added_items_counter} items to the inventory")
     print("Skipped:")
     for item_name, item_count in skipped.items():
-        print(item_name + " " + str(item_count))
+        print(item_name, item_count)
     print("===============")
+
     return inventory
 
 
-stuff = add_to_inventory(stuff, dragonLoot)
+stuff = add_to_inventory(stuff, dragon_loot)
 
 display_inventory(stuff)
