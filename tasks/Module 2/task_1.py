02bd9a03cdb2f027900433f31cdd72f78daf6859
GeorgiiDermenzhy <george.dermenzhy@gmail.com>
2020-06-12 20:10:02 +0200

    Make changes based on pull request comments: 1) create UNWANTED_ITEMS const 2) fix weight CONST place to start check with heaviest 3) remove comments

--- tasks/Module 2/task_1.py
+++ tasks/Module 2/task_1.py
@@ -1,13 +1,14 @@
 """Task 1 and 2 of Module 2."""
 
 from collections import defaultdict
-# create CONSTANT variables for weight
+
 LIGHT_WEIGHT_THRESHOLD = 60
 HEAVY_WEIGHT_THRESHOLD = 70
 MAX_INVENTORY_CAPACITY = 80
+UNWANTED_ITEMS = ['rubbish', 'chewed gum', 'used tissue']
 
 stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
-# update dragonLoot variable changed to dragon_loot
+
 dragon_loot = [
 
     'gold coin', 'chewed gum', 'dagger', 'gold coin', 'gold coin', 'ruby',
@@ -16,49 +17,46 @@ dragon_loot = [
 
 
 def display_inventory(inventory):
-    # change header for display_inventory function
-    """Check the inventory of a player and provides weight/speed dependency status."""
-    print("Inventory:")
+    """Check the inventory and provides weight/speed dependency status."""
     item_total = 0
 
+    print("Inventory:")
+
     for item_name, item_count in inventory.items():
-        # change print iteams , to use default print() separator settings
+
         print(item_name, item_count)
-        # change item_total counter to use augmented assignment
+
         item_total += 1
 
     print("Total number of items: " + str(item_total))
-    # here notify our hero if he/she is carrying too much
-    if item_total >= LIGHT_WEIGHT_THRESHOLD:
-        print(
-            "CAUTION: Your backpack weighs a lot, "
-            "your stamina runs out quicker!")
+
+    if item_total >= MAX_INVENTORY_CAPACITY:
+        print("CAUTION: You are overloaded, can't move!")
     elif item_total >= HEAVY_WEIGHT_THRESHOLD:
         print(
             "CAUTION: Your equipment is very heavy, "
             "you're moving slower than usual!")
-    elif item_total >= MAX_INVENTORY_CAPACITY:
-        print("CAUTION: You are overloaded, can't move!")
+    elif item_total >= LIGHT_WEIGHT_THRESHOLD:
+        print(
+            "CAUTION: Your backpack weighs a lot, "
+            "your stamina runs out quicker!")
 
 
 def add_to_inventory(inventory, added_items):
     """Add to inventory.
 
     - Adds new items to inventory of a player
-    - Checks input items for trash
+    - Checks input items for unwanted_items
     - Provides information on skipped added iteams
     """
     skipped = {}
-    # add implementation of defaultdict for skipped dict
+
     skipped = defaultdict(int)
 
     added_items_counter = 0
 
-    # your code goes here
-    trash = ['rubbish', 'chewed gum', 'used tissue']
-
     for item in added_items:
-        if item in trash:
+        if item in UNWANTED_ITEMS:
             skipped[item] += 1
         elif item in inventory:
             inventory[item] += 1
@@ -66,9 +64,11 @@ def add_to_inventory(inventory, added_items):
         else:
             inventory[item] = 1
             added_items_counter += 1
-    # change print for number of added items, to use f-strings
+
     print(f"Added {added_items_counter} items to the inventory")
+
     print("Skipped:")
+
     for item_name, item_count in skipped.items():
         print(item_name, item_count)
     print("===============")
