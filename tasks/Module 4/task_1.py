dc479064a31123dee1f34c32d73762c703354ea1
GeorgiiDermenzhy <george.dermenzhy@gmail.com>
2020-07-31 18:25:58 +0200

    Module 4 task 1: Change of looping through line.split() to splitted_sentence that has list of of words splitted by using regular expression

--- tasks/Module 4/task_1.py
+++ tasks/Module 4/task_1.py
@@ -1,6 +1,7 @@
 """Task 1 of Module 4."""
 from pathlib import Path
 import pyinputplus as pyip
+import re
 
 WORDS_TO_REPLACE = ["ADJECTIVE", "NOUN", "VERB"]
 
@@ -19,23 +20,21 @@ def mad_libs():
             input_file.write("The ADJECTIVE panda walked to the NOUN and then VERB. "
                              "A nearby NOUN was unaffected by these events.")
 
-    with open(input_file_path, "r") as input_file:
-        with open(output_file_path, "w") as output_file:
-            for line in input_file:
-                for word in line.split():
-                    if word in WORDS_TO_REPLACE:
-                        input_word = pyip.inputStr(f"Please provide {word}:\n")
-                        new_text.append(input_word)
-                    elif word[:len(word) - 1] in WORDS_TO_REPLACE:
-                        input_word = pyip.inputStr(f"Please provide {word[:len(word) - 1]}:\n")
-                        new_text.append(input_word + word[-1])
-                    else:
-                        new_text.append(word)
-            output_file.write(" ".join(new_text))
+    with open(input_file_path, "r") as input_file, open(output_file_path, "w") as output_file:
+        for line in input_file:
+            slitted_sentence = re.findall(r"[\w']+|\.|\s", line)
+            for word in slitted_sentence:
+                if word in WORDS_TO_REPLACE:
+                    input_word = pyip.inputStr(f"Please provide {word}:\n")
+                    new_text.append(input_word)
+                elif word[:len(word) - 1] in WORDS_TO_REPLACE:
+                    input_word = pyip.inputStr(f"Please provide {word[:len(word) - 1]}:\n")
+                    new_text.append(input_word + word[-1])
+                else:
+                    new_text.append(word)
+        output_file.write("".join(new_text))
 
-    with open(output_file_path, "r") as input_file:
-        rf_content = input_file.read()
-        print(rf_content)
+    print("".join(new_text))
 
 
 mad_libs()
